043deb44fda2ed00e4ecb4d39d7c6e9f9132792a
Shionoya <shionoya.akari@gmail.com>
2023-12-21 20:36:17 +0900

    feat:foreachの追加

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,12 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+
+//期待する出力になるようにコードを追加しましょう(foreach文を使用してください)
+// 1番目: ブラジルが入っています。
+// 2番目: 日本が入っています。
+// 3番目: スペインが入っています。
+
+
+$countries = ["ブラジル", "日本", "スペイン"];
+foreach ($countries as $number => $countryName ){
+  echo $number +1 . "番目：" . $countryName . "が入っています。" . "<br>";
+}
